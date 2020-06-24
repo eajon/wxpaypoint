5c88519be21c9c583f3712873797edcf1c3e8f6e
eajon <wengyijiong@csfz.cn>
2020-06-24 15:33:21 +0800

    * 子更新

---
+++ app/src/main/java/cn/csfz/wxpaypoint/util/SystemCtrlUtil.java
@@ -0,0 +1,168 @@
+package cn.csfz.wxpaypoint.util;
+
+import android.content.Context;
+
+import java.io.DataOutputStream;
+import java.io.PrintWriter;
+
+public class SystemCtrlUtil {
+
+    /**
+     * root下静默安装
+     * */
+    public static boolean rootSlienceInstallApk(String apkPath){
+        PrintWriter PrintWriter = null;
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            PrintWriter = new PrintWriter(process.getOutputStream());
+            PrintWriter.println("chmod 777 "+apkPath);
+            PrintWriter.println("export LD_LIBRARY_PATH=/vendor/lib:/system/lib");
+            PrintWriter.println("pm install -r "+apkPath);
+//          PrintWriter.println("exit");
+            PrintWriter.flush();
+            PrintWriter.close();
+            int value = process.waitFor();
+            return returnResult(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(process!=null){
+                process.destroy();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * root下启动apk
+     * */
+    public static boolean rootStartApk(String packageName,String activityName){
+        boolean isSuccess = false;
+        String cmd = "am start -n " + packageName + "/" + activityName + " \n";
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(cmd);
+            int value = process.waitFor();
+            return returnResult(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            if(process!=null){
+                process.destroy();
+            }
+        }
+        return isSuccess;
+    }
+
+
+
+    /**
+     * root下静默删除
+     * */
+    public static boolean rootSlienceUninstallApk(String packageName){
+        PrintWriter PrintWriter = null;
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            PrintWriter = new PrintWriter(process.getOutputStream());
+            PrintWriter.println("LD_LIBRARY_PATH=/vendor/lib:/system/lib ");
+            PrintWriter.println("pm uninstall "+packageName);
+            PrintWriter.flush();
+            PrintWriter.close();
+            int value = process.waitFor();
+            return returnResult(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(process!=null){
+                process.destroy();
+            }
+        }
+        return false;
+    }
+
+
+
+    /**
+     * 判断手机是否有root权限
+     */
+    public static boolean sysHasRootPerssion(){
+        PrintWriter PrintWriter = null;
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            PrintWriter = new PrintWriter(process.getOutputStream());
+            PrintWriter.flush();
+            PrintWriter.close();
+            int value = process.waitFor();
+            return returnResult(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(process!=null){
+                process.destroy();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * root下执行cmd的返回值
+     * */
+    private static boolean returnResult(int value){
+        // 代表成功
+        if (value == 0) {
+            return true;
+        } else if (value == 1) { // 失败
+            return false;
+        } else { // 未知情况
+            return false;
+        }
+    }
+
+
+    /**
+     * 判断app是否有root权限
+     */
+    public static boolean appHasRootPerssion(Context context){
+        return RootCommand("chmod 777 "+context.getPackageCodePath());
+    }
+
+
+    /**
+     * 应用程序运行命令获取 Root权限，设备必须已破解(获得ROOT权限)
+     * @param command 命令：String apkRoot="chmod 777 "+getPackageCodePath(); RootCommand(apkRoot);
+     * @return 应用程序是/否获取Root权限
+     */
+    public static boolean RootCommand(String command)
+    {
+        Process process = null;
+        DataOutputStream os = null;
+        try
+        {
+            process = Runtime.getRuntime().exec("su");
+            os = new DataOutputStream(process.getOutputStream());
+            os.writeBytes(command + "\n");
+            os.writeBytes("exit\n");
+            os.flush();
+            return returnResult(process.waitFor());
+        } catch (Exception e)
+        {
+            return false;
+        } finally
+        {
+            try
+            {
+                if (os != null)
+                {
+                    os.close();
+                }
+                process.destroy();
+            } catch (Exception e)
+            {
+            }
+        }
+    }
+
+}
