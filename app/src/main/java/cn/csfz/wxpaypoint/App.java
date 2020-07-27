b6fd3b9ca3b3d27261e8eced05d1b5ff0add7c35
eajon <wengyijiong@csfz.cn>
2020-07-27 15:22:43 +0800

    * 改为https

--- app/src/main/java/cn/csfz/wxpaypoint/App.java
+++ app/src/main/java/cn/csfz/wxpaypoint/App.java
@@ -1,9 +1,7 @@
 package cn.csfz.wxpaypoint;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.os.Build;
 import android.os.RemoteException;
@@ -11,14 +9,10 @@ import android.os.RemoteException;
 import androidx.annotation.RequiresApi;
 
 import com.danikula.videocache.HttpProxyCacheServer;
-import com.github.eajon.RxConfig;
 import com.github.eajon.RxHttp;
 import com.github.eajon.util.LoggerUtils;
-import com.google.gson.Gson;
 import com.microsoft.signalr.HubConnection;
 import com.microsoft.signalr.HubConnectionBuilder;
-import com.microsoft.signalr.HubConnectionState;
-import com.microsoft.signalr.TransportEnum;
 import com.sunfusheng.daemon.DaemonHolder;
 import com.tencent.wxpayface.IWxPayfaceCallback;
 import com.tencent.wxpayface.WxPayFace;
@@ -28,27 +22,27 @@ import java.io.File;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import cn.csfz.wxpaypoint.activity.CloseDoorActivity;
-import cn.csfz.wxpaypoint.activity.OpenDoorActivity;
-import cn.csfz.wxpaypoint.model.VersionModel;
-import cn.csfz.wxpaypoint.util.ActivityCollector;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 import cn.csfz.wxpaypoint.util.Utils;
-import cn.eajon.tool.ActivityUtils;
-import cn.eajon.tool.AppUtils;
-import cn.eajon.tool.DeviceUtils;
 import cn.eajon.tool.LogUtils;
-import cn.eajon.tool.ObservableUtils;
-import es.dmoral.toasty.Toasty;
-import io.reactivex.Observable;
-import io.reactivex.functions.Consumer;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import top.wuhaojie.installerlibrary.AutoInstaller;
 
 public class App extends Application implements Thread.UncaughtExceptionHandler {
 
     public static HubConnection hubConnection;
     private static Application self;
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -57,6 +51,7 @@ public class App extends Application implements Thread.UncaughtExceptionHandler 
         self = this;
         Thread.setDefaultUncaughtExceptionHandler(this);
         UMConfigure.init(App.this, "5f1a8825d62dd10bc71bda16", "csfz", UMConfigure.DEVICE_TYPE_PHONE, null);
+
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
                 .addNetworkInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
                     @Override
@@ -67,7 +62,8 @@ public class App extends Application implements Thread.UncaughtExceptionHandler 
                         .setLevel(HttpLoggingInterceptor.Level.BASIC))
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .readTimeout(60, TimeUnit.SECONDS)
-                .writeTimeout(60, TimeUnit.SECONDS);
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .sslSocketFactory(sslSocketFactory(),x509TrustManager());
 
         RxHttp.getConfig()
                 .baseUrl(BuildConfig.SERVER_URL)
@@ -131,6 +127,38 @@ public class App extends Application implements Thread.UncaughtExceptionHandler 
     public void uncaughtException(Thread thread, Throwable ex) {
         Utils.restartAPP(self);
         System.exit(0);
+    }
+
+    public X509TrustManager x509TrustManager() {
+        return new X509TrustManager() {
+            @SuppressLint("TrustAllX509TrustManager")
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @SuppressLint("TrustAllX509TrustManager")
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        };
+    }
+
+
+    public SSLSocketFactory sslSocketFactory() {
+        try {
+            //信任任何链接
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{x509TrustManager()}, new SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
